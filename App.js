3a7c9f30bbd1190fc53c120a78e9ff3a01d9f50f
Andreas Jensen <Andreasbachjensen4200@gmail.com>
2023-09-20 13:14:34 +0200

    Update App.js
    Co-Authored-By: Jeppe Bøgeskov <jepbe123@gmail.com>

--- App.js
+++ App.js
@@ -1,11 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
+import React from 'react';
+
 
 export default function App() {
+  const [text, onChangeText] = React.useState('');
+  const [Psswd, onChangePsswd] = React.useState('');
+  
   return (
     <View style={styles.container}>
-      <Text styles={[styles.headerText]}>Seb er fed</Text>
       <Image source={require('./assets/FitBattleLogo.jpg')} style={styles.mImage} />
+      <Text style={styles.headerText}>Login</Text>
+      <TextInput 
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+        placeholder={"username"}
+      />
+      <TextInput 
+        style={styles.input}
+        onChangeText={onChangePsswd}
+        value={Psswd}
+        placeholder={"password"}
+        secureTextEntry={true}
+      />
+
+      <TouchableOpacity 
+        onPress={() =>{}} 
+        style={{
+          backgroundColor: '#AD40AF', 
+          padding: 18, 
+          borderRadius: 10, 
+          marginBottom: 30,
+          width: 150, 
+          }}>
+        <Text 
+          style={{
+            textAlign: 'center',
+            fontWeight: '700',
+            fontSize: 16,
+            color: '#fff',
+          }}>
+            Login
+          </Text>
+      </TouchableOpacity>
+
+
       <StatusBar style="auto" />
     </View>
   );
@@ -27,5 +67,12 @@ const styles = StyleSheet.create({
 
   headerText: {
     fontSize: 30,
-  }
+  },
+
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
 });
